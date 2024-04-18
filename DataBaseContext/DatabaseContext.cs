4a79ddb25e46d0dc118173aa1d0ca4ab356266e4
Sayem Ahamed <95583680+Sayemahamed@users.noreply.github.com>
2024-04-18 13:22:32 +0600

    Add project files.

---
+++ DataBaseContext/DatabaseContext.cs
@@ -0,0 +1,13 @@
+﻿using ClassAssignment.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClassAssignment.DataBaseContext;
+
+public class DatabaseContext : DbContext
+{
+    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+    public DbSet<Students> Students
+    {
+        get; set;
+    }
+}
---
+++ Entities/Students.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClassAssignment.Entities;
+
+public class Students
+{
+    [Key]
+    string StudentId
+    {
+        get; set;
+    } = null!;
+    public string StudentName
+    {
+        get; set;
+    } = string.Empty;
+    public string StudentPhone
+    {
+        get; set;
+    } = string.Empty;
+    public string StudentEmail
+    {
+        get; set;
+    } = string.Empty;
+    public string Department
+    {
+        get; set;
+    } = string.Empty;
+    public int semester
+    {
+        get; set;
+    }
+}
